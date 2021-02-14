21806389ce8426f87a8e88f8df348667ed68d275
martijnxd <martijn.ploeg@gmail.com>
2021-02-14 23:05:58 +0100

    added cobra viper

--- gituser/add.go
+++ gituser/add.go
@@ -3,27 +3,36 @@ package gituser
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "add github user",
-	Long:  `add github user`,
-	Run: func(cmd *cobra.Command, args []string) {
-		adduser()
-	},
+type gituserdata struct {
+	userName  string `mapstructure:"user_name"`
+	userEmail string `mapstructure:"user_email"`
+	userToken string `mapstructure:"user_token"`
 }
 
-func init() {
-	rootCmd.AddCommand(addCmd)
+type gitusers struct {
+	users      gituserdata
+	configfile string `mapstructure:"config_file"`
 }
 
-func adduser() {
+func Adduser() {
+	var userdata gituserdata
 	fmt.Println("add user")
 	fmt.Println("Enter username: ")
-	var username string
-	_, err := fmt.Scanln(&username)
+	_, err := fmt.Scanln(&userdata.userName)
+	fmt.Println("Enter username: ")
+	_, err = fmt.Scanln(&userdata.userEmail)
+	fmt.Println("Enter username: ")
+	_, err = fmt.Scanln(&userdata.userToken)
+
+	if userdata.userName != "" {
+		viper.SetDefault("users.user_name", userdata.userName)
+		viper.SetDefault("users.user_email", userdata.userEmail)
+		viper.SetDefault("users.user_token", userdata.userToken)
+		viper.WriteConfig()
+	}
 
 	if err != nil {
 		error.Error(err)
