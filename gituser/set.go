346f7becb87eb0a8932c4e538a9e3a17811ba584
martijnxd <martijn.ploeg@gmail.com>
2021-09-02 18:56:23 +0200

    get .ssh path based on os

--- gituser/set.go
+++ gituser/set.go
@@ -6,9 +6,19 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/spf13/viper"
 )
+
+func getsshpath() (ssh_path string) {
+	if runtime.GOOS == "windows" {
+		ssh_path = os.Getenv("userprofile") + "\\.ssh\\"
+	} else {
+		ssh_path = os.Getenv("HOME") + "/.ssh/"
+	}
+	return ssh_path
+}
 
 func Copyfile(src, dst string) error {
 	in, err := os.Open(src)
@@ -39,9 +49,9 @@ func Setuser(username string) {
 			_, err = exec.Command("git", "config", "--local", "user.email", usr.Email).Output()
 			_, err = exec.Command("git", "config", "--global", "user.name", usr.Name).Output()
 			_, err = exec.Command("git", "config", "--global", "user.email", usr.Email).Output()
-
-			src := os.Getenv("userprofile") + "\\.ssh\\" + usr.Keyfilename
-			dest := os.Getenv("userprofile") + "\\.ssh\\" + "id_rsa"
+			ssh_path := getsshpath()
+			src := ssh_path + usr.Keyfilename
+			dest := ssh_path + "id_rsa"
 			e := Copyfile(src, dest)
 			if e != nil {
 				log.Fatal(e)
